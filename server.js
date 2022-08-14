8857e21fb91b68c4bde7350c49a2681d48b64242
goldbearman <46929880+goldbearman@users.noreply.github.com>
2022-08-14 22:52:42 +0300

    post

--- server.js
+++ server.js
@@ -1,43 +1,65 @@
 const experess = require('express');
 const path = require('path');
+const morgan = require('morgan')
 
 const app = experess();
 
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 
 const PORT = 3000;
-const createPath = (page) => path.resolve(__dirname,'ejs-views', `${page}.ejs`); //разные платформы-разная /,поэтому используем
+const createPath = (page) => path.resolve(__dirname, 'ejs-views', `${page}.ejs`); //разные платформы-разная /,поэтому используем
 
 app.listen(PORT, 'localhost', error => {            //имя хоста обязательно! localhost
   error ? console.log(error) : console.log(`listening port ${PORT}`);
 });
 
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
+
+app.use(experess.urlencoded({extends: false}));
+
+app.use(experess.static('styles'));
+
 //Отправляем html
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
   res.render(createPath('index'));   //index.ejs
 });
 
-app.get('/contacts',(req,res)=>{
+app.get('/contacts', (req, res) => {
   res.render(createPath('contacts'));   //contacts.ejs
 });
 
-app.get('/posts/:id',(req,res)=>{
-  res.render(createPath('post'));   //contacts.ejs
+app.get('/posts/:id', (req, res) => {
+  const title = 'Post';
+  const post = {
+    id: 1,
+    text: 'dfs',
+    date: '05.08.2022',
+    author: 'I am'
+  };
+  res.render(createPath('post'), {title, post});   //contacts.ejs
 });
 
-app.get('/posts',(req,res)=>{
+app.post('/add-posts', (req, res) => {
+  const {title, author, text} = req.body;
+  const post ={
+    text,author, title
+  };
+  res.render(createPath('post'),{post, title});
+});
+
+app.get('/posts', (req, res) => {
   res.render(createPath('posts'));   //contacts.ejs
 });
 
-app.get('/add-post',(req,res)=>{
+app.get('/add-post', (req, res) => {
   res.render(createPath('add-posts'));   //contacts.ejs
 });
 
-app.get('/about-us',(req,res)=>{
+app.get('/about-us', (req, res) => {
   res.redirect('/contacts');   //contacts.ejs
 });
 
-app.use((req,res)=>{ //должен быт в конце!!!
+app.use((req, res) => { //должен быт в конце!!!
   res
     .status(404)                           //передаем статускод
     .render(createPath('error'))         //error.ejs
